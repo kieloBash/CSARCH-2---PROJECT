5b4e93cd0b9844c147c6302ebc37f3e6639d6b46
kieloBash <114970278+kieloBash@users.noreply.github.com>
2023-03-28 10:21:42 +0800

    created the base of the project

---
+++ front-end/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./scenes/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <main className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/src/scenes/Home.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+
+const Home = () => {
+  
+  return (
+  <div>
+    <h1>IEEE-754 Decimal-32 floating point translator</h1>
+  </div>
+  );
+};
+
+export default Home;
